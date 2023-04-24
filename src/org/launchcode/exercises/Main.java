de46480b3c11d4315144b9989559e14d621113ee
Darren LaCour <lacourd@icloud.com>
2023-04-23 21:01:15 -0500

    Finished chapter 11 exercises

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -7,7 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Test out your Divide() function!
+        Divide(10, 2);
+        Divide(5,2);
+//        Divide(10, 0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -15,17 +17,34 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
-        // Test out your CheckFileExtension() function!
+        for (Map.Entry<String, String> student : studentFiles.entrySet()) {
+            System.out.println(student.getKey() + ": " + CheckFileExtension(student.getValue()));
+        }
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static double Divide(int x, int y) {
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("You cannot divide by zero!");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+        return x/y;
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
-    }
-
+    public static int CheckFileExtension(String fileName) {
+        if (fileName.contains(".java")) {
+            return 1;
+        } else if (fileName.equals(null) || fileName.equals("")) {
+            try {
+                throw new Exception("You didn't submit anything!");
+            } catch (Exception e) {
+                e.printStackTrace();
+                return -1;
+            }
+        } else {
+                return 0;
+            }
+        }
 }
